35dead05afb36f98ca188b0d766a2dfc0bc99cc8
Anas Adnane <aadnane@e2r5p15.1337.ma>
2022-05-22 20:36:33 +0100

    i guess it's the last push

--- utils_bin_handle.c
+++ utils_bin_handle.c
@@ -6,35 +6,11 @@
 /*   By: aadnane <aadnane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 18:02:21 by aadnane           #+#    #+#             */
-/*   Updated: 2022/05/21 14:47:46 by aadnane          ###   ########.fr       */
+/*   Updated: 2022/05/22 20:10:20 by aadnane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-int	is_bin(char c)
-{
-	if (c == '0')
-		return (0);
-	else
-		return (1);
-}
-
-int	ft_bin_to_dec(char *str)
-{
-	int		i;
-	int		res;
-
-	i = 0;
-	res = 0;
-	while (str[i])
-	{
-		res *= 2;
-		res = res + is_bin(str[i]);
-		i++;
-	}
-	return (res);
-}
 
 void	ft_dec_to_bin(char c, int pid)
 {
@@ -55,19 +31,4 @@ void	ft_dec_to_bin(char c, int pid)
 		c = c << 1;
 		i++;
 	}
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	size_t			i;
-	unsigned char	*str;
-
-	i = 0;
-	str = (unsigned char *)s;
-	while (i < n)
-	{
-		str[i] = '\0';
-		i++;
-	}
-	s = str;
 }
